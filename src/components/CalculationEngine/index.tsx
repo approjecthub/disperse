a7a36529683cf26e46e3bc4d7a13d585220feb24
approjecthub <anirbanpal1997@gmail.com>
2023-09-20 01:00:41 +0530

    duplicate entry handling, error action logic implemented

--- src/components/CalculationEngine/index.tsx
+++ src/components/CalculationEngine/index.tsx
@@ -1,17 +1,25 @@
 import { useState } from "react";
 import TextArea from "../TextArea";
 import styles from "./index.module.css";
+import {
+  CALCULATION_DUPLICATION_ACTION_TYPE,
+  CALCULATION_ERROR_TYPE,
+} from "./constant";
 
 const CalculationEngine = (): JSX.Element => {
   const [val, setVal] = useState<string>("");
   const [calculationEngineError, setCalculationEngineError] = useState<
-    CalculationEngineErrorMessage[]
-  >([]);
+    | CalculationEngineInvalidDataErrorMessage
+    | CalculationEngineDuplicatedDataErrorMessage
+    | null
+  >(null);
 
   const validAddressPattern = /^0x[a-fA-F0-9]{40}$/i;
+  const validAmountPattern = /^[0-9]+$/;
 
   const onChange = (arg: string) => {
     setVal(arg);
+    setCalculationEngineError(null);
   };
 
   const handleValidate = () => {
@@ -23,12 +31,10 @@ const CalculationEngine = (): JSX.Element => {
 
       //validate current line data length
       if (!Array.isArray(currentLineDatas) || currentLineDatas.length !== 2) {
-        setCalculationEngineError((prev) => [
-          ...prev,
-          {
-            lineNumbers: [idx + 1],
-          },
-        ]);
+        setCalculationEngineError({
+          errorType: CALCULATION_ERROR_TYPE.INVALID_DATA,
+          lineNumber: idx + 1,
+        });
         return;
       }
 
@@ -37,23 +43,19 @@ const CalculationEngine = (): JSX.Element => {
 
       //validate the address
       if (!validAddressPattern.test(address)) {
-        setCalculationEngineError((prev) => [
-          ...prev,
-          {
-            lineNumbers: [idx + 1],
-          },
-        ]);
+        setCalculationEngineError({
+          errorType: CALCULATION_ERROR_TYPE.INVALID_DATA,
+          lineNumber: idx + 1,
+        });
         return;
       }
 
       //validate the amount
-      if (!/[0-9]/.test(amount)) {
-        setCalculationEngineError((prev) => [
-          ...prev,
-          {
-            lineNumbers: [idx + 1],
-          },
-        ]);
+      if (!validAmountPattern.test(amount)) {
+        setCalculationEngineError({
+          errorType: CALCULATION_ERROR_TYPE.INVALID_DATA,
+          lineNumber: idx + 1,
+        });
         return;
       }
 
@@ -65,23 +67,133 @@ const CalculationEngine = (): JSX.Element => {
       }
     }
 
-    const duplicateAddressErrors: CalculationEngineErrorMessage[] = [];
+    const duplicateAddressErrors: CalculationEngineDuplicatedDataErrorMessage =
+      {
+        errorType: CALCULATION_ERROR_TYPE.DUPLICATE_DATA,
+        addressToLineNumbersMap: {},
+      };
     Object.keys(addressToLineNumberMap).forEach((address) => {
       if (addressToLineNumberMap[address].length > 1) {
-        duplicateAddressErrors.push({
-          lineNumbers: addressToLineNumberMap[address],
-        });
+        duplicateAddressErrors.addressToLineNumbersMap = {
+          ...duplicateAddressErrors.addressToLineNumbersMap,
+          [address]: addressToLineNumberMap[address],
+        };
       }
     });
 
-    if (duplicateAddressErrors.length > 0) {
+    if (
+      Object.keys(duplicateAddressErrors.addressToLineNumbersMap).length > 0
+    ) {
       setCalculationEngineError(duplicateAddressErrors);
     }
+  };
+
+  const handleDuplicateErrors = (
+    actionType: CALCULATION_DUPLICATION_ACTION_TYPE
+  ) => {
+    const allDatas: string[] = val.split(/[\n,=]/);
+    const addressToAmountMap: { [key: string]: number } = {};
+
+    allDatas.forEach((data) => {
+      const currentLineDatas = data.split(" ");
+      const address = currentLineDatas[0];
+      const amount = +currentLineDatas[1];
+
+      if (addressToAmountMap[address]) {
+        if (actionType === CALCULATION_DUPLICATION_ACTION_TYPE.COMBINE_ALL) {
+          addressToAmountMap[address] += amount;
+        }
+      } else {
+        addressToAmountMap[address] = amount;
+      }
+    });
+
+    setVal(
+      Object.keys(addressToAmountMap)
+        .map((address) => {
+          return `${address} ${addressToAmountMap[address]}`;
+        })
+        .join("\n")
+    );
+    setCalculationEngineError(null);
+  };
+
+  const getErrorMessgae = () => {
+    if (!calculationEngineError) return null;
+
+    if (
+      calculationEngineError.errorType === CALCULATION_ERROR_TYPE.INVALID_DATA
+    ) {
+      return (
+        <p>
+          {`Line ${
+            (calculationEngineError as CalculationEngineInvalidDataErrorMessage)
+              .lineNumber
+          } wrong amount`}
+        </p>
+      );
+    } else if (
+      calculationEngineError.errorType === CALCULATION_ERROR_TYPE.DUPLICATE_DATA
+    ) {
+      const { addressToLineNumbersMap } =
+        calculationEngineError as CalculationEngineDuplicatedDataErrorMessage;
+      return (
+        <>
+          {Object.keys(addressToLineNumbersMap).map((address) => (
+            <p>
+              {`Address ${address} encountered duplicate in Line: ${addressToLineNumbersMap[address]}`}
+            </p>
+          ))}
+        </>
+      );
+    } else return null;
   };
 
   return (
     <div className={styles.calculation_engine_container}>
       <TextArea value={val} onChange={onChange} />
+      {calculationEngineError && (
+        <div className={styles.calculation_engine_error_container}>
+          {calculationEngineError.errorType ===
+            CALCULATION_ERROR_TYPE.DUPLICATE_DATA && (
+            <div
+              className={
+                styles.calculation_engine_duplicate_error_action_container
+              }
+            >
+              <p>Duplicated</p>
+              <p>
+                <button
+                  className={styles.calculation_engine_action}
+                  onClick={() =>
+                    handleDuplicateErrors(
+                      CALCULATION_DUPLICATION_ACTION_TYPE.KEEP_FIRST
+                    )
+                  }
+                >
+                  Keep the first one
+                </button>
+                |
+                <button
+                  className={styles.calculation_engine_action}
+                  onClick={() =>
+                    handleDuplicateErrors(
+                      CALCULATION_DUPLICATION_ACTION_TYPE.COMBINE_ALL
+                    )
+                  }
+                >
+                  Combine Balance
+                </button>
+              </p>
+            </div>
+          )}
+          <div
+            className={styles.calculation_engine_error_description_container}
+          >
+            {getErrorMessgae()}
+          </div>
+        </div>
+      )}
       <button
         onClick={handleValidate}
         className={styles.calculation_engine_validate_button}
--- src/components/CalculationEngine/type.d.ts
+++ src/components/CalculationEngine/type.d.ts
@@ -1,3 +1,13 @@
-declare type CalculationEngineErrorMessage = {
-  lineNumbers: number[];
-};
+declare interface CalculationEngineErrorMessage {
+  errorType: CALCULATION_ERROR_TYPE;
+}
+
+declare interface CalculationEngineInvalidDataErrorMessage
+  extends CalculationEngineErrorMessage {
+  lineNumber: number;
+}
+
+declare interface CalculationEngineDuplicatedDataErrorMessage
+  extends CalculationEngineErrorMessage {
+  addressToLineNumbersMap: { [key: string]: number[] };
+}
